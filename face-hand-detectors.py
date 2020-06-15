e9e837a2ae43762d08fe8f701dd4e25a96e9fa65
keerthisri24 <64601701+keerthisri24@users.noreply.github.com>
2020-06-15 17:01:04 -0500

    Create face-hand-detectors.py

---
+++ face-hand-detectors.py
@@ -0,0 +1,30 @@
+import os
+import numpy as np
+import cvlib as cv
+import tensorflow as tf
+from shapely.geometry import polygon
+
+
+class Detector:
+    detector_params = {}
+    detector = None
+
+    def __init__(self):
+        pass
+
+    def set_detector_params(self, params):
+        self.detector_params = params
+
+    def detect(self):
+        pass
+
+
+class CVLibDetector(Detector):
+    def __init__(self):
+        self.detector = cv
+
+    def detect(self, rgb_image):
+        # returns an array of coordinates (top, bottom, right left)
+        faces, confidences = self.detector.detect_face(rgb_image)
+        # change to array of (x, y, w, h)
+        return [(top, left, bottom - top, right - left) for (top, right, bottom, left) in faces]
